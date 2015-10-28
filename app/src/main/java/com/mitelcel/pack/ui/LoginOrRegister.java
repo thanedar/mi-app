557afdf7858476a523b5020e82cf7dee6b030a28
Sudhanshu <sudhanshu.thanedar@acotel.com>
2015-10-28 18:20:48 -0400

    Refactored the packages and classes.

--- app/src/main/java/com/mitelcel/pack/ui/LoginOrRegister.java
+++ app/src/main/java/com/mitelcel/pack/ui/LoginOrRegister.java
@@ -28,21 +28,6 @@ public class LoginOrRegister extends BaseActivityLogin implements OnDialogListen
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void clickWidgetOnFragment(View view) {
         switch (view.getId()) {
             case R.id.signin_tv_new_user://1
